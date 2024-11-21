d7c5eeb892590f5ef0f47a8ad3b42412d3d5897b
arifadnan05 <arifahamedadnan@gmail.com>
2024-11-21 20:52:46 +0600

    Navbar and footer setup and file structure be organized

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,9 @@
-import Image from "next/image";
+import Homepage from "./components/homepage/Homepage";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Hello This is Car doctor</h1>
-      <button className="btn btn-accent">Button</button>
+    <div className="">
+      <Homepage />
     </div>
   );
 }
