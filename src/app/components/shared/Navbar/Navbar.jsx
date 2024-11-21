65d28026579db4e46086627cc12eb7a75f765eb8
arifadnan05 <arifahamedadnan@gmail.com>
2024-11-22 00:22:45 +0600

    navbar all text and navigation added

--- src/app/components/shared/Navbar/Navbar.jsx
+++ src/app/components/shared/Navbar/Navbar.jsx
@@ -1,57 +1,38 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
-
+import { FiShoppingCart } from "react-icons/fi";
+import { FaMagnifyingGlass } from "react-icons/fa6";
 const Navbar = () => {
+
     return (
-        <div className="navbar bg-base-100">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-5 w-5"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor">
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M4 6h16M4 12h8m-8 6h16" />
-                        </svg>
-                    </div>
-                    <ul
-                        tabIndex={0}
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+        <div className='bg-base-100'>
+            <div className="navbar container mx-auto px-4">
+                <div className="navbar-start">
+                    <Link className='btn btn-ghost' href={'/'}>
+                        <Image alt='logo' src='/assets/logo.svg' height={40} width={80} />
+                    </Link>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        <div className='flex space-x-6'>
+                            <Link href={'/'}><li className='font-semibold hover:text-primary duration-300'>Home</li></Link>
+                            <Link href={'/about'}><li className='font-semibold hover:text-primary duration-300'>About</li></Link>
+                            <Link href={'/services'}><li className='font-semibold hover:text-primary duration-300'>Services</li></Link>
+                            <Link href={'/blog'}><li className='font-semibold hover:text-primary duration-300'>Blog</li></Link>
+                            <Link href={'/contact'}><li className='font-semibold hover:text-primary duration-300'>Contact</li></Link>
+                        </div>
+
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
-            </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
-                </ul>
-            </div>
-            <div className="navbar-end">
-                <a className="btn">Button</a>
+                <div className="navbar-end">
+                    <div className='flex space-x-8 mr-6'>
+                        <span className='text-xl'><FiShoppingCart /></span>
+                        <span className='text-xl'><FaMagnifyingGlass /></span>
+                    </div>
+
+                    <a className="btn btn-outline border-primary hover:bg-primary text-primary">Appointment</a>
+                </div>
             </div>
         </div>
     )
