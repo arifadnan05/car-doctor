619f30e49b7cad436a76ec8becf90b93bcb859dd
arifadnan05 <arifahamedadnan@gmail.com>
2024-11-27 23:26:18 +0600

    Signup page succes and data will be send on mongoDB

--- src/app/(authentication)/signup/api/route.js
+++ src/app/(authentication)/signup/api/route.js
@@ -1,42 +1,6 @@
-// import { connectDB } from "@/lib/connectDB";
-
-// export const POST = async (request) => {
-//     try {
-//         // Parse the request body
-//         const newUser = await request.json();
-
-//         // Connect to the database
-//         const db = await connectDB();
-//         const usersCollection = db.collection("users");
-
-//         // Check if the user already exists
-//         const exist = await usersCollection.findOne({ email: newUser.email });
-//         if (exist) {
-//             return new Response(
-//                 JSON.stringify({ message: "User already exists" }),
-//                 { status: 409, headers: { "Content-Type": "application/json" } }
-//             );
-//         }
-
-//         // Insert the new user
-//         await usersCollection.insertOne(newUser);
-//         return new Response(
-//             JSON.stringify({ message: "User created successfully" }),
-//             { status: 201, headers: { "Content-Type": "application/json" } }
-//         );
-//     } catch (error) {
-//         // Log the error for debugging
-//         console.error("Error in POST /api/users:", error);
-
-//         return new Response(
-//             JSON.stringify({ message: "Something went wrong", error: error.message }),
-//             { status: 500, headers: { "Content-Type": "application/json" } }
-//         );
-//     }
-// };
-
 
 import { connectDB } from "@/lib/connectDB";
+import bcrypt from "bcrypt";
 
 export const POST = async (request) => {
     try {
@@ -51,8 +15,9 @@ export const POST = async (request) => {
                 { status: 304, headers: { "Content-Type": "application/json" } }
             );
         }
+        const hashedPassword = bcrypt.hashSync(newUser.password, 14);
 
-        const res = await usersCollection.insertOne(newUser);
+        const res = await usersCollection.insertOne({ ...newUser, password: hashedPassword });
         return new Response(
             JSON.stringify({ message: "User Created", userId: res.insertedId }),
             { status: 201, headers: { "Content-Type": "application/json" } }
